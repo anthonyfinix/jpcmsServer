6c5dacae0737e2edc634f01dbf3c71d07a525829
Anthony Finix <anthonyfinix@gmail.com>
2021-10-17 12:55:14 +0530

    updated ui for dialogbox ui

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -6,27 +6,27 @@ self.addEventListener("activate", (e) => {
         caches.keys().then(cacheNames => {
             return Promise.all(
                 cacheNames.map(cache => {
-                    if (cache !== cacheName) {
+                    // if (cache !== cacheName) {
                         return caches.delete(cache)
-                    }
+                    // }
                 })
             )
         })
     )
 });
 self.addEventListener("fetch", (e) => { 
-    e.respondWith(
-        fetch(e.request)
-            .then(response => {
-                let responseClone = response.clone();
-                if (e.request.url.indexOf('http') === 0) {
-                    caches.open(cacheName)
-                        .then(cache => {
-                            cache.put(e.request, responseClone)
-                        })
-                }
-                return response;
-            })
-            .catch(() => caches.match(e.request).then(res => res))
-    )
+    // e.respondWith(
+    //     fetch(e.request)
+    //         .then(response => {
+    //             let responseClone = response.clone();
+    //             if (e.request.url.indexOf('http') === 0) {
+    //                 caches.open(cacheName)
+    //                     .then(cache => {
+    //                         cache.put(e.request, responseClone)
+    //                     })
+    //             }
+    //             return response;
+    //         })
+    //         .catch(() => caches.match(e.request).then(res => res))
+    // )
 });
